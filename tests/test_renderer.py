dd4b3006e98f8ed03955df91977f89624003ecda
Hsiaoming Yang <me@lepture.com>
2018-11-27 01:42:24 +0900

    Add more test cases

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -1,3 +1,4 @@
+import pedantmark
 from pedantmark import markdown, HTMLRenderer
 
 
@@ -41,3 +42,77 @@ def test_image():
 
     rv = markdown('![a](<javascript:void> "b")', renderer=HTMLRenderer())
     assert rv.strip() == '<p><!-- dangerous image --></p>'
+
+
+def test_thematic_break():
+    rv = markdown('***', renderer=HTMLRenderer())
+    assert rv.strip() == '<hr />'
+
+
+def test_block_quote():
+    rv = markdown('> a', renderer=HTMLRenderer())
+    assert rv.strip() == '<blockquote><p>a</p>\n</blockquote>'
+
+
+def test_code_block():
+    rv = markdown('    a\n', renderer=HTMLRenderer())
+    assert rv.strip() == '<pre><code>a\n</code></pre>'
+
+    rv = markdown('```c\na\n```', renderer=HTMLRenderer())
+    assert rv.strip() == '<pre><code class="language-c">a\n</code></pre>'
+
+    def highlight(code, lang):
+        return b'test'
+
+    rv = markdown('```c\na\n```', renderer=HTMLRenderer(highlight))
+    assert rv == 'test'
+
+
+def test_ordered_list():
+    rv = markdown('1. a', renderer=HTMLRenderer())
+    assert rv.strip() == '<ol>\n<li>a</li>\n</ol>'
+
+    rv = markdown('5. a', renderer=HTMLRenderer())
+    assert rv.strip() == '<ol start="5">\n<li>a</li>\n</ol>'
+
+
+def test_unordered_list():
+    rv = markdown('- a', renderer=HTMLRenderer())
+    assert rv.strip() == '<ul>\n<li>a</li>\n</ul>'
+
+
+def test_footnote():
+    s = '[^1]\n\n[^1]: a'
+    rv = markdown(
+        s, renderer=HTMLRenderer(),
+        options=[pedantmark.OPT_FOOTNOTES]
+    )
+    assert 'id="fn' in rv
+    assert 'class="footnotes"' in rv
+
+
+TABLE_TEXT_1 = '''
+a1 | a2
+-- | --
+b1 | b2
+'''
+
+TABLE_TEXT_2 = '''
+a1  | a2
+:-- | :--:
+b1  | b2
+'''
+
+
+def test_table():
+    rv = markdown(
+        TABLE_TEXT_1, renderer=HTMLRenderer(),
+        extensions=['table'],
+    )
+    assert '<th>' in rv
+
+    rv = markdown(
+        TABLE_TEXT_2, renderer=HTMLRenderer(),
+        extensions=['table'],
+    )
+    assert 'style=' in rv
